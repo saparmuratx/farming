734685ffd35d464cbd6b7a9d5d2288e56d36b866
Saparmurat <saparmurat@protonmail.com>
2023-02-11 01:48:05 +0600

    add Plot serializer

---
+++ farms/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework_gis import serializers
+
+from .models import Plot
+
+
+class PlotSerializer(serializers.GeoFeatureModelSerializer):
+    class Meta:
+        model = Plot
+        geo_field = "contour"
+        fields = (
+            "farmer",
+            "culture",
+            "season",
+        )
