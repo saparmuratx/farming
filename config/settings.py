2255a88c7f2f4a18e2191d6fc600eeb50fa46985
Saparmurat <saparmurat@protonmail.com>
2023-02-11 01:47:27 +0600

    setup rest framework

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.gis",
+    # third-party
+    "rest_framework",
+    "rest_framework_gis",
     # local
     "farms",
 ]
