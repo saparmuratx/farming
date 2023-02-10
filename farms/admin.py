c6b67b5848a7cd14f56b6221ad455caca6e4f9e5
Saparmurat <saparmurat@protonmail.com>
2023-02-10 19:54:23 +0600

    register Plot model

--- farms/admin.py
+++ farms/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 
 from .models import (
     Farmer,
     Culture,
     Season,
+    Plot,
 )
 
 
 admin.site.register(Farmer)
 admin.site.register(Culture)
 admin.site.register(Season)
+
+
+@admin.register(Plot)
+class PlotAdmin(OSMGeoAdmin):
+    list_display = ("contour", "farmer", "culture", "season")
