6aca033d068f41bc566ffa3d3a206f2a01cc503a
Saparmurat <saparmurat@protonmail.com>
2023-02-12 01:57:53 +0600

    add permissions to PlotView

--- farms/views.py
+++ farms/views.py
@@ -1,11 +1,17 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework_gis.pagination import GeoJsonPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import PlotSerializer
 from .models import Plot
+from .permissions import IsAuthorOrReadOnly
 
 
-class PlotListView(ListAPIView):
+class PlotView(ModelViewSet):
     queryset = Plot.objects.all()
     pagination_class = GeoJsonPagination
     serializer_class = PlotSerializer
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly,
+    )
