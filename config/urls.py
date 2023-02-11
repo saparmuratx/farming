b080e2d2155698c7ae71b7ae5632d6655b6b4c2f
Saparmurat <saparmurat@protonmail.com>
2023-02-12 01:56:29 +0600

    setup urls

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/plots/", include("farms.urls")),
+    path("api/", include("farms.urls")),
     path("api/auth/", include("rest_framework.urls")),
     path("api/accounts/", include("drf_registration.urls")),
 ]
